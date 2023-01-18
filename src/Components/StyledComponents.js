e96269885297f218ba7866c5ae9d6de52ad13f63
MirkusX <marcusk.kodehode@gmail.com>
2023-01-18 13:46:27 +0100

    added example for testing

---
+++ src/Components/StyledComponents.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StyledSection = styled.section`
+  display: flex;
+`;
+
+export const StyledDiv = styled.div`
+  display: flex;
+  width: 50%;
+`;
+
+export const StyledImage = styled.image`
+  width: 50%;
+`;
--- src/Pages/Frontpage.js
+++ src/Pages/Frontpage.js
@@ -1,24 +1,57 @@
 import { useReducer, useState } from "react";
+import {
+  StyledDiv,
+  StyledForm,
+  StyledImage,
+  StyledSection,
+} from "../Components/StyledComponents";
 import { initialState, reducer } from "../Components/useReducer";
+import biznizcard from "../image/biznizcard.webp";
 
 export const Frontpage = () => {
   const [info, setInfo] = useState([]);
   const [state, dispatch] = useReducer(reducer, initialState);
-  console.log(state.name);
   return (
-    <section>
-      <div>
+    <>
+      <header>
         <h1>contact page</h1>
-      </div>
-      <form>
-        <input
-          type="text"
-          onInput={(e) => dispatch({ type: "name", payload: e.target.value })}
-        />
-        <input type="email" />
-        <input type="number" />
-        <input type="url" />
-      </form>
-    </section>
+      </header>
+      <StyledSection>
+        <StyledDiv>
+          <StyledImage src={biznizcard} />
+          <div>
+            <h2>name</h2>
+            <h2>number</h2>
+            <h2>email</h2>
+          </div>
+        </StyledDiv>
+        <StyledForm>
+          <input
+            type="text"
+            onInput={(e) => dispatch({ type: "name", payload: e.target.value })}
+          />
+          <input
+            type="email"
+            onInput={(e) =>
+              dispatch({ type: "email", payload: e.target.value })
+            }
+          />
+          <input
+            type="number"
+            onInput={(e) =>
+              dispatch({ type: "number", payload: e.target.value })
+            }
+          />
+          <input
+            type="url"
+            onInput={(e) => dispatch({ type: "url", payload: e.target.value })}
+          />
+          <input
+            type="file"
+            onInput={(e) => dispatch({ type: "url", payload: e.target.value })}
+          />
+        </StyledForm>
+      </StyledSection>
+    </>
   );
 };
